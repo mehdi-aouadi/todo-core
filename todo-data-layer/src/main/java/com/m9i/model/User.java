776d082f3c4a6775200bbf6a54093d014ba9ef6c
mehdi-aouadi <mehdi.aouadi@gmail.com>
2018-08-04 19:33:46 +0200

    [3] Add lombok annotations to entities

--- todo-data-layer/src/main/java/com/m9i/model/User.java
+++ todo-data-layer/src/main/java/com/m9i/model/User.java
@@ -1,8 +1,14 @@
 package com.m9i.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Document(collection = "User")
 public class User {
 
@@ -12,33 +18,8 @@ public class User {
     private String email;
     private String phoneNumber;
 
-    public User() {}
-
     public User(String email, String phoneNumber) {
         this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
