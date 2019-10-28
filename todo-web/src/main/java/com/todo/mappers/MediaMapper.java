676583c0d807987e64b3822dbe4e2b30dfbfa421
mehdi-aouadi <mehdi.aouadi@gmail.com>
2019-10-28 13:21:49 +0100

    [83][MEDIA] Add Media paging

--- todo-web/src/main/java/com/todo/mappers/MediaMapper.java
+++ todo-web/src/main/java/com/todo/mappers/MediaMapper.java
@@ -1,8 +1,12 @@
 package com.todo.mappers;
 
+import com.todo.common.Page;
 import com.todo.contents.MediaContent;
+import com.todo.contents.MediaPageContent;
 import com.todo.model.Media;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -16,5 +20,10 @@ public interface MediaMapper {
   MediaContent domainToContent(Media media);
   List<MediaContent> domainListToContentList(List<Media> mediaList);
   Media contentToDomain(MediaContent mediaContent);
+
+  @Mappings( {
+      @Mapping(source = "totalElementCount", target = "totalCount"),
+  })
+  MediaPageContent pageToMediaPageContent(Page<Media> mediaPage);
 
 }
